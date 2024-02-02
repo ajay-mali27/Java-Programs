d4e1ca199f3fede3391d5e890eda263af382b4fc
Ajay Mali <ajaymali2701@gmail.com>
2024-02-02 20:36:27 +0530

    Swapping Numbers

---
+++ J3_SwapNum.java
@@ -0,0 +1,24 @@
+public class J3_SwapNum {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+
+        // Before Swapping;
+
+        System.out.println("Before Swapping");
+        System.out.println(a);
+        System.out.println(b);
+
+        // After Swapping;
+
+        int temp = a;  
+        a = b;
+        b = temp;
+
+        // Display The Value After The Swapping;
+
+        System.out.println("After Swapping");
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+}
