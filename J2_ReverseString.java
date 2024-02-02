8c7339927040042bae623d5ee82bb6f5c4fbbf17
Ajay Mali <ajaymali2701@gmail.com>
2024-02-02 20:36:08 +0530

    Reverse String

---
+++ J2_ReverseString.java
@@ -0,0 +1,16 @@
+public class J2_ReverseString {
+
+    public static void main(String[] args) {
+
+        String name = "12345";
+        int strLen = name.length();
+        String voidStr = " ";
+
+        for (int i = strLen - 1; i >= 0; i--) {
+
+            voidStr = voidStr + name.charAt(i);
+        }
+        System.out.println("The Reverse String is =>" + voidStr);
+        
+    }
+}
