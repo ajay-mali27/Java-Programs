c97b4549a58bfa52b9b6af34ef1c90a3ac5bc652
Ajay Mali <ajaymali2701@gmail.com>
2024-02-05 19:03:26 +0530

    do changes

--- J7_Palindrome.java
+++ J7_Palindrome.java
@@ -7,9 +7,23 @@ public class J7_Palindrome {
         Scanner SC = new Scanner(System.in);
         System.out.println("Enter the Palindrome Number");
         int num = SC.nextInt();
-        System.out.println("You Entered =>" + num + " <=Number");
+        // System.out.println("You Entered =>" + num + " <=Number");
         int rem;
         int temp = num;
+
+        int rev = 0;
+
+        while (temp != 0) {
+            rem = temp % 10; //
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+
+        if (num == rev) {
+            System.out.println(num + " =>  Palindrome Number");
+        } else {
+            System.out.println(num + " =>Not palindrome");
+        }
 
     }
 
